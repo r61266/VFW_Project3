e0ea508ad24f9898a048c583bb263a1b93800fc0
r61266 <lightking@fullsail.edu>
2013-03-21 05:36:32 -0400

    updating

--- JS/Main.js
+++ JS/Main.js
@@ -1,36 +1,81 @@
 // Alec Shallenberger
 // March 13, 2013
 // Term 1303
-// VFW Project2
+// VFW Project3
+
+window.addEventListener("DOMContentLoaded", function(){
+		
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+	
+	function getSelectedRadio(){
+		var radios = document.forms[0].sex;
+		for (var i=0; i<radios.length; i++){
+			if(radios[i].checked){
+				sexValue = radios[i].value;
+			}
+		}
+	}
+
+	function storeData(){
+		var id					= Math.floor(Math.random()*10000000001);
+		getSelectedRadio();
+		var item				= {};
+			item.fname 			= ["First Name", $('fname').value];
+			item.lname			= ["Last Name", $('lname').value];
+			item.date			= ["Date", $('date').value];
+			item.email			= ["Email", $('email').value];
+			item.age			= ["Age", $('age').value];
+			item.addr			= ["Home Address", $('addr').value];
+			/*item.Favorites		= ["Check Favorites:", favoriteValue ];*/
+			item.sex			= ["Sex:", sexValue];
+			item.Artist			= ["Preferred Artist", $('Artist').value];
+			item.notes			= ["Additional Notes", $('notes').value];
+		
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Birthday Saved!");
+	}
+	
+	function getData(){
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		document.appendChild(makeDiv);
+		for(var i=0, len=localStorage.length; i<len; i++){
+			var makeli = document.createElement('li');
+			makeList.appendChild(makeli);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			var object = JSON.parse(value);
+			var makeSubList = document.createElement('ul');
+			makeli.appendChild(makeSubList);
+			for(var t in object){
+				varmakeSubli = document.createElement('li');
+				makeSubList.appendChild(makeSubli);
+				var optSubText = object[t][0]+" "+object[t][1];
+				makeSubli.innerHTML = optSubText;
+			}
+		} 
+	}
+	 var sexValue;
+	
+	
+	
+	var displayLink = $('displayLink');
+	displayLink.addEventListener("click", getData);
+	/*var clearLink = $('clear');
+	clearLink.addEventListener("click", clearLocal);*/
+	var save = $('submit');
+	save.addEventListener("click", storeData);
+
+});
 
 
-var fname = document.getElementById("fname");
-var lname = document.getElementById("lname");
-var date = document.getElementById("date");
-var email = document.getElementById("email");
-var age = document.getElementById("age");
-var addr = document.getElementById("addr");
 
-var captureData = function(){
-	localStorage.setItem("First Name", fname.value);
-	localStorage.setItem("Last Name", lname.value);
-	localStorage.setItem("Date of Birth", date.value);
-	localStorage.setItem("Email", email.value);
-	localStorage.setItem("Age", age.value);
-	localStorage.setItem("Home Address", addr.value);
-};
-//EventListeners 
-fname.addEventListener("blur", captureData);
-lname.addEventListener("blur", captureData);
-date.addEventListener("blur", captureData);
-email.addEventListener("blur", captureData);
-age.addEventListener("blur", captureData);
-addr.addEventListener("blur", captureData);
 
-for(i=0, j=localStorage.length; i<j; i++){
-	var get = localStorage.key(i);
-	var retrieve = localStorage.getItem(get);
-	console.log(retrieve+" : " +get);
-};
+
 
 
